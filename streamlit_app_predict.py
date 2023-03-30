8ec6fa6c97f0f87b8eaffaa55ddf7de7958171f4
kishuinvain1 <37648429+kishuinvain1@users.noreply.github.com>
2023-03-30 18:32:14 +0530

    Update streamlit_app_predict.py

--- streamlit_app_predict.py
+++ streamlit_app_predict.py
@@ -49,7 +49,8 @@ def segFormCrack(cl, x, y, w, h, cnf, saved_image):
     y = int(y)
     w = int(w)
     h = int(h)
-    roi = img[y-h//2+200:y+h//2-200, x-w//2+200:x+w//2-200, :]
+    bias = 100
+    roi = img[y-h//2+bias:y+h//2-bias, x-w//2+bias:x+w//2-bias, :]
     #st.image(roi, caption="ROI")
     cv2.imwrite("saved_ROI.jpg", roi)
     segform_model = loadSegFormModel()
