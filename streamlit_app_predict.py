3343711931ee6bfac0fabd0179e24108b0ae7522
kishuinvain1 <37648429+kishuinvain1@users.noreply.github.com>
2023-03-30 17:38:11 +0530

    Update streamlit_app_predict.py

--- streamlit_app_predict.py
+++ streamlit_app_predict.py
@@ -11,28 +11,22 @@ import numpy as np
 
 
 def load_image():
-    opencv_image = None
-    
+    opencv_image = None 
     path = None
     f = None
     uploaded_file = st.file_uploader(label='Pick an image to test')
     print(uploaded_file)
     if uploaded_file is not None:
-        
-        image_data = uploaded_file.getvalue()
-        
-
-      
+        file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+        opencv_image = cv2.imdecode(file_bytes, -1)
+        image_data = uploaded_file.getvalue() 
         #st.image(image_data)
         name = uploaded_file.name
         path = os.path.abspath(name)
         print("abs path")
         print(path)
-        with open(uploaded_file.name,'wb') as f:
-            f.write(uploaded_file.read())
-
-    return path, f
-        #print(Path.cwd())
+    return path, opencv_image
+       
 
 
 	
@@ -44,8 +38,8 @@ def loadSegFormModel():
     return model
 	
 def segFormCrack(cl, x, y, w, h, cnf, saved_image):
-    img = cv2.imread(saved_image)
-    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    #img = cv2.imread(saved_image)
+    img = cv2.cvtColor(saved_image,cv2.COLOR_BGR2RGB)
     x = int(x)
     y = int(y)
     w = int(w)
@@ -62,8 +56,8 @@ def drawBoundingBox(saved_image ,x, y, w, h, cl, cf):
     #img = Image.open(saved_image)
     
 
-    img = cv2.imread(saved_image)
-    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    #img = cv2.imread(saved_image)
+    img = cv2.cvtColor(saved_image,cv2.COLOR_BGR2RGB)
     x = int(x)
     y = int(y)
     w = int(w)
@@ -75,15 +69,9 @@ def drawBoundingBox(saved_image ,x, y, w, h, cl, cf):
         cl = "Non-Broken"
     
     img = cv2.rectangle(img, start_pnt, end_pnt, (0,255,0), 10)
-    img = cv2.putText(img, cl, txt_start_pnt, cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 10, cv2.LINE_AA)
-
-	
-    
-	
+    img = cv2.putText(img, cl, txt_start_pnt, cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 10, cv2.LINE_AA)	
     st.image(img, caption='Resulting Image')	
     
-	
-
 
 
 def predict(model, url):
@@ -99,14 +87,7 @@ def main():
     #rf = Roboflow(api_key="SNxIPCnRCYWXUM9lBAp4")
     #project = rf.workspace().project("fleet-crack-2-wg5qy")
     #model = project.version(1).model
-    
-    
-
-
-# infer on an image hosted elsewhere
-# print(model.predict("URL_OF_YOUR_IMAGE", hosted=True, confidence=40, overlap=30).json())
-    
-    
+     
     image, svd_img = load_image()
     #st.write('Enter the image URL')
     #url = st.text_input('URL', '')
@@ -117,7 +98,7 @@ def main():
         rf2 = Roboflow(api_key="uhDFc9G6MKjrEvbfHt6B")
         project2 = rf2.workspace().project("fleetguard")
         model2 = project2.version(1).model
-        results = predict(model2, svd_img.name)
+        results = predict(model2, svd_img)
         #results = predict(model2, url)
         print("Prediction Results are...")	
         print(results)
@@ -134,14 +115,14 @@ def main():
             cl = results['predictions'][0]['class']
             cnf = results['predictions'][0]['confidence']
             print("printing saved image")
-            print(svd_img.name)
+            #print(svd_img.name)
 	
             #st.image(svd_img, "saved image")
-            drawBoundingBox(svd_img.name,x, y, w, h, cl, cnf)
+            drawBoundingBox(svd_img,x, y, w, h, cl, cnf)
             #st.write(cl)
             #st.write(cnf)
             if(cl == "Crack" or cl == "No-Crack"):
-                sem_seg_res = segFormCrack(cl, x, y, w, h, cnf, svd_img.name)
+                sem_seg_res = segFormCrack(cl, x, y, w, h, cnf, svd_img)
 
     elif(result and option == "Zoomed-in"):
         st.write('Calculating results...')
